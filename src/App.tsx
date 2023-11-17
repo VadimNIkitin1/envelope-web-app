a2682c1c95b6f3368f23f3576c7cf22f261c7956
VadimNikitin1 <vchivas18years@gmail.com>
2023-11-17 16:01:32 +0300

    check spalsh screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { useTelegram } from './hooks/useTelegram';
 import router from './router';
 import style from './App.module.scss';
 
 function App() {
+  const [preloader, setPreloader] = useState(true);
   const { tg } = useTelegram();
 
   useEffect(() => {
     tg.expand();
   }, []);
 
+  setTimeout(() => {
+    setPreloader(false);
+  }, 1500);
+
   return (
     <div className={style.app}>
+      <div className={preloader ? style.preloader : style.preloader_done}>
+        <div className={preloader ? style.loader : style.loader_done}>ENVELOPE</div>
+      </div>
       <RouterProvider router={router} />
     </div>
   );
