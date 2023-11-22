eb01f5ac4332935691f1a0ef72b15d43ca72f033
VadimNikitin1 <vchivas18years@gmail.com>
2023-11-22 11:47:41 +0300

    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { useTelegram } from './hooks/useTelegram';
 import router from './router';
